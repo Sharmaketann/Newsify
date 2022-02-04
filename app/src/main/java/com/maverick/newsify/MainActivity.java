da2026f4c380b5ef8a3cdbd71695b1efe914164d
Sharma Ketan <sharmaketann@gmail.com>
2022-02-04 10:48:46 +0530

    Initial commit

---
+++ app/src/main/java/com/maverick/newsify/MainActivity.java
@@ -0,0 +1,15 @@
+package com.maverick.newsify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //cb0ecff5821d4ca2a2e4e09490f0ac50
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
